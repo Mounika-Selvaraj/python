08007274abbc5412149d72b8a48bb265c552fd8a
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-19 17:43:05 +0530

    Create Find the Runner-Up Score.py

---
+++ Find the Runner-Up Score.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+if __name__ == '__main__':
+    n = int(input())
+    arr= list(map(int, input().split()))   
+    a= max(arr) 
+    c=arr.count(a)
+    for i in range(c):
+        arr.remove(a)
+    print(max(arr))
